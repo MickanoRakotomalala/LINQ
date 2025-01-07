a522d6227125f8055a478bccb92cf4c9907465a8
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2025-01-07 07:51:50 +0300

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Application Console pour voir le fonctionnement du LINQ");
+        Console.WriteLine("Grouper des nombres par parité :");
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        var groups = from n in numbers
+                     group n by n % 2 into g
+                     select new { key = g.Key, numbers = g };
+
+        foreach (var group in groups)
+        {
+            Console.WriteLine(group.key == 0 ? "Even:" : "Odd");
+            foreach (var num in group.numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
